0bd5289d36354b6fcc20fb221d15e840e726b07f
Tijmen Wierenga <tijmen.wierenga@persgroep.net>
2021-11-05 16:45:51 +0100

    Added rector to automate refactoring

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src'
+    ]);
+
+    // Define what rule sets will be applied
+    $containerConfigurator->import(SetList::DEAD_CODE);
+
+    // get services (needed for register a single rule)
+    $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+    $services->set(FinalizePublicClassConstantRector::class);
+    $services->set(FinalizeClassesWithoutChildrenRector::class);
+};
