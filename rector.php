07916ede230f2c8fd656e264c98f25dc3e4aa37c
TijmenWierenga <t.wierenga@live.nl>
2021-11-17 21:56:31 +0100

    Updated Rector config

--- rector.php
+++ rector.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
@@ -15,9 +17,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src'
     ]);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 
     // Define what rule sets will be applied
     $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
 
     // get services (needed for register a single rule)
     $services = $containerConfigurator->services();
@@ -26,4 +31,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // $services->set(TypedPropertyRector::class);
     $services->set(FinalizePublicClassConstantRector::class);
     $services->set(FinalizeClassesWithoutChildrenRector::class);
+    $services->set(NoUnusedImportsFixer::class);
 };
